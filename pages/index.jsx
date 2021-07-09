3cafee8e6783b0b71595e0c892b7709ffa2135da
Fawaz Haroun <harounayomikun@gmail.com>
2021-07-09 10:52:56 +0100

    update

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,13 @@
 import React from "react"
+import Link from "next/link"
 
 const Page = () => {
   return (
     <div>
-      Index Page
+      <h1>Index Page</h1>
+      <Link href="/notes">
+        <a>Go to notes</a>
+      </Link>
     </div>
   )
 }
--- pages/notes/index.jsx
+++ pages/notes/index.jsx
@@ -1,9 +1,13 @@
 import React from "react"
+import Link from "next/link"
 
 const Page = () => {
   return (
     <div>
-      Note Index Path
+      <h1>Note Index Path</h1>
+      <Link href="/notes/[id]" as={"/notes/1"}>
+          <a>Note 1 Page</a>
+      </Link>
     </div>
   )
 }
