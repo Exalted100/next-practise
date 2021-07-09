51ed2e4e4746b423477483bea9187e4a0367bafe
Fawaz Haroun <harounayomikun@gmail.com>
2021-07-09 19:11:09 +0100

    commit

--- pages/notes/[id].jsx
+++ pages/notes/[id].jsx
@@ -1,15 +1,33 @@
 import React from "react"
 import { useRouter } from "next/router"
 
-const Page = () => {
-    const router = useRouter()
-    const { id } = router.query
+const Page = ({ note }) => {
+    console.log(note)
 
     return (
         <div>
-            Note {id}
+            Note: {note.title}
         </div>
     )
 }
 
 export default Page
+
+export async function getServerSideProps({params, req, res}) {
+    const response = await fetch(`http://localhost:3000/api/note/${params.id}`)
+  
+    if (!response.ok) {
+      res.writeHead(302, { Location: '/notes' })
+      res.end()
+      return {props: {}}
+    }
+  
+    const {data} = await response.json()
+    
+    
+    if (data) {
+      return {
+        props: {note: data}
+      }
+    }
+  }
--- pages/notes/index.jsx
+++ pages/notes/index.jsx
@@ -1,15 +1,30 @@
 import React from "react"
 import Link from "next/link"
 
-const Page = () => {
+const Page = ({ notes }) => {
   return (
     <div>
       <h1>Note Index Path</h1>
-      <Link href="/notes/[id]" as={"/notes/1"}>
-          <a>Note 1 Page</a>
-      </Link>
+      {console.log(notes)}
+      {notes.map(note => (
+            <div key={note.id}>
+                <Link href="/notes/[id]" as={`/notes/${note.id}`}>
+                    <a>{note.title} Page</a>
+                </Link>
+            </div>
+      ))}
     </div>
   )
+}
+
+export async function getServerSideProps() {
+    const res = await fetch(`http://localhost:3000/api/note/`)
+    const { data } = await res.json()
+  
+    console.log(data)
+    return {
+      props: {notes: data}
+    }
 }
 
 export default Page
